ceb89571a8bbb89b74d51a7d341c0d72a6669da9
YashJain2409 <yashsj24@gmail.com>
2022-03-08 12:29:10 +0530

    their message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -10,7 +10,7 @@ const ChatFeed = (props) => {
     return keys.map((key, index) => {
       const message = messages[key];
       const lastMessageKey = index === 0 ? null : keys[index - 1];
-      const isMyMessage = userName === message.sender.userName;
+      const isMyMessage = userName === message.sender.username;
       return (
         <div key={`msg_${index}`} style={{ width: "100%" }}>
           <div className="message-block">
@@ -29,8 +29,9 @@ const ChatFeed = (props) => {
               marginRight: isMyMessage ? "18px" : "0px",
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
-            // readReciepts
-          ></div>
+          >
+            reciepts
+          </div>
         </div>
       );
     });
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,4 +1,5 @@
 const MyMessage = ({ message }) => {
+  let doc = new DOMParser().parseFromString(message.text, "text/xml");
   if (message?.attachments?.length > 0) {
     return (
       <img
@@ -18,7 +19,9 @@ const MyMessage = ({ message }) => {
         color: "white",
         backgroundColor: "#3b2a50",
       }}
-    >{message.text}</div>
+    >
+      {message.text !== "" && doc.firstChild.innerHTML}
+    </div>
   );
 };
 export default MyMessage;
